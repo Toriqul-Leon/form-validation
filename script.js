0aa623ba01ee96f2fb1e08e5941bb4732265674b
Toriqul-Leon <yeitoriqul@gmail.com>
2022-03-09 22:08:04 +0600

    Checked length, Email and Password Match. All Done

--- script.js
+++ script.js
@@ -18,12 +18,14 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 // !Check valid Mail
-const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+const checkEmail = (input) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Email is not Valid`);
+  }
 };
 
 // !Check Required Fields
@@ -36,7 +38,28 @@ function checkRequired(inputArray) {
     }
   });
 }
-
+// !Check input Length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} character`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+// !Check Password
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Password do not match");
+  }
+}
 // !Get Field Name
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -46,6 +69,10 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, confirmPassword]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, confirmPassword);
 
   //   if (username.value === "") {
   //     showError(username, "Username is required");
