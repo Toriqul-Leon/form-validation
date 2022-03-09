b650a9e80bafa25f13749340745db1e7df374409
Toriqul-Leon <yeitoriqul@gmail.com>
2022-03-09 21:28:12 +0600

    Refactoring done

--- script.js
+++ script.js
@@ -25,32 +25,50 @@ const validateEmail = (email) => {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 };
+
+// !Check Required Fields
+function checkRequired(inputArray) {
+  inputArray.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is Required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// !Get Field Name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 // !Events
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
+  checkRequired([username, email, password, confirmPassword]);
 
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!validateEmail(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSuccess(email);
-  }
+  //   if (username.value === "") {
+  //     showError(username, "Username is required");
+  //   } else {
+  //     showSuccess(username);
+  //   }
 
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else {
-    showSuccess(password);
-  }
-  if (confirmPassword.value === "") {
-    showError(confirmPassword, "Password is required");
-  } else {
-    showSuccess(confirmPassword);
-  }
+  //   if (email.value === "") {
+  //     showError(email, "Email is required");
+  //   } else if (!validateEmail(email.value)) {
+  //     showError(email, "Email is not valid");
+  //   } else {
+  //     showSuccess(email);
+  //   }
+
+  //   if (password.value === "") {
+  //     showError(password, "Password is required");
+  //   } else {
+  //     showSuccess(password);
+  //   }
+  //   if (confirmPassword.value === "") {
+  //     showError(confirmPassword, "Password is required");
+  //   } else {
+  //     showSuccess(confirmPassword);
+  //   }
 });
